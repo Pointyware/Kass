af4997c5ecd04ae97b56561085c2b3d60ce988be
Taush Sampley <taushsampley@gmail.com>
2024-03-22 20:27:33 -0500

    [FEATURE] Add Test Assumptions/Assertions (#2)
    * Initial Commit
    
    Generate Project
    
    * Transfer configuration and files from painted-dogs:assertions

---
+++ assertions/src/commonTest/kotlin/ScenarioTest.kt
@@ -0,0 +1,27 @@
+package org.pointyware.kass.assertions
+
+import org.pointyware.kass.assertions.FailedAssumption
+import org.pointyware.kass.assertions.assume
+import org.pointyware.kass.assertions.runTestWith
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.fail
+
+/**
+ */
+class ScenarioTest {
+    @Test
+    fun `failed assumptions within test scenario prevent any further execution and allow tests to pass`() {
+        runTestWith("") {
+            assume().that(subject).isNotEmpty()
+            fail("This should not be reached")
+        }
+    }
+
+    @Test
+    fun `failed assumptions outside test scenario are not caught and cause the test to fail`() {
+        assertFailsWith<FailedAssumption> {
+            assume().that("").isNotEmpty()
+        }
+    }
+}
