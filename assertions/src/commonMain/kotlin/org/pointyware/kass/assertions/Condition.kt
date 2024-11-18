ed996fc2a4059bf5e47b751a90622f7547ff3258
Taush Sampley <taushsampley@gmail.com>
2024-11-18 13:24:26 -0600

    Move collection condition to dedicated file

--- assertions/src/commonMain/kotlin/org/pointyware/kass/assertions/Condition.kt
+++ assertions/src/commonMain/kotlin/org/pointyware/kass/assertions/Condition.kt
@@ -75,28 +75,6 @@ class StringCondition(
 }
 
 /**
- * Extends [Condition] to provide additional statements for collections.
- */
-class CollectionCondition<T>(
-    override val subject: Collection<T>,
-    asserter: Asserter
-): Condition<Collection<T>>(subject, asserter) {
-    fun contains(item: T): CollectionCondition<T> {
-        asserter.assertTrue("$subject does not contain $item", item in subject)
-        return this
-    }
-
-    fun doesNotContain(item: T): CollectionCondition<T> {
-        asserter.assertTrue("$subject contains $item", item !in subject)
-        return this
-    }
-
-    override fun and(): CollectionCondition<T> {
-        return this
-    }
-}
-
-/**
  * Extends [Condition] to provide additional statements for maps.
  */
 class MapCondition<K, V>(
