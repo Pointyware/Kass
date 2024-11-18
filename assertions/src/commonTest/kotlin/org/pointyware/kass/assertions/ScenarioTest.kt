89c8c66f187180ff1a9fe01a91dc8cbdedf5df81
Taush Sampley <taushsampley@gmail.com>
2024-11-18 16:29:56 -0600

    update scenario test

--- assertions/src/commonTest/kotlin/org/pointyware/kass/assertions/ScenarioTest.kt
+++ assertions/src/commonTest/kotlin/org/pointyware/kass/assertions/ScenarioTest.kt
@@ -1,5 +1,6 @@
 package org.pointyware.kass.assertions
 
+import org.pointyware.kass.assertions.primitive.StringStatements.isNotEmpty
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
 import kotlin.test.fail
@@ -14,7 +15,7 @@ class ScenarioTest {
     @Test
     fun failed_assumptions_within_test_scenario_prevent_any_further_execution_and_allow_tests_to_pass() {
         runScenarioWith(emptySubjectList) {
-            assume.that(subject).isNotEmpty()
+            assumeThat(subject, isNotEmpty())
             fail("This should not be reached")
         }
     }
@@ -22,7 +23,7 @@ class ScenarioTest {
     @Test
     fun failed_assumptions_within_hypothesis_prevent_any_further_execution_and_allow_tests_to_pass() {
         testHypothesis {
-            assume.that("").isNotEmpty()
+            assumeThat("", isNotEmpty())
             fail("This should not be reached")
         }
     }
@@ -30,7 +31,7 @@ class ScenarioTest {
     @Test
     fun failed_assumptions_outside_test_scenario_are_not_caught_and_cause_the_test_to_fail() {
         assertFailsWith<FailedAssumption> {
-            assume.that("").isNotEmpty()
+            assumeThat("", isNotEmpty())
         }
     }
 }
