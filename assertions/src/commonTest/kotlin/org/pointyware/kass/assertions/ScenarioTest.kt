fa661a39a65c0bcb3ac8471b988acd887acb3481
Taush Sampley <taushsampley@gmail.com>
2024-11-18 15:36:49 -0600

    replace assume function with val

--- assertions/src/commonTest/kotlin/org/pointyware/kass/assertions/ScenarioTest.kt
+++ assertions/src/commonTest/kotlin/org/pointyware/kass/assertions/ScenarioTest.kt
@@ -17,7 +17,7 @@ class ScenarioTest {
     @Test
     fun failed_assumptions_within_test_scenario_prevent_any_further_execution_and_allow_tests_to_pass() {
         runScenarioWith(emptySubjectList) {
-            assume().that(subject).isNotEmpty()
+            assume.that(subject).isNotEmpty()
             fail("This should not be reached")
         }
     }
@@ -25,7 +25,7 @@ class ScenarioTest {
     @Test
     fun failed_assumptions_within_hypothesis_prevent_any_further_execution_and_allow_tests_to_pass() {
         testHypothesis {
-            assume().that("").isNotEmpty()
+            assume.that("").isNotEmpty()
             fail("This should not be reached")
         }
     }
@@ -33,7 +33,7 @@ class ScenarioTest {
     @Test
     fun failed_assumptions_outside_test_scenario_are_not_caught_and_cause_the_test_to_fail() {
         assertFailsWith<FailedAssumption> {
-            assume().that("").isNotEmpty()
+            assume.that("").isNotEmpty()
         }
     }
 }
