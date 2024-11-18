46d64af42fb3dc9c5380aa505a0a61f6bbee15c5
Taush Sampley <taushsampley@gmail.com>
2024-11-18 15:11:17 -0600

    Add collections and map condition empty unit tests

---
+++ assertions/src/commonTest/kotlin/org/pointyware/kass/assertions/collections/CollectionConditionUnitTest.kt
@@ -0,0 +1,31 @@
+package org.pointyware.kass.assertions.collections
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+/**
+ *
+ */
+class CollectionConditionUnitTest {
+
+    @BeforeTest
+    fun setUp() {
+
+    }
+
+    @AfterTest
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun contains() {
+
+    }
+
+    @Test
+    fun doesNotContain() {
+
+    }
+}
---
+++ assertions/src/commonTest/kotlin/org/pointyware/kass/assertions/collections/MapConditionUnitTest.kt
@@ -0,0 +1,31 @@
+package org.pointyware.kass.assertions.collections
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+/**
+ *
+ */
+class MapConditionUnitTest {
+
+    @BeforeTest
+    fun setUp() {
+
+    }
+
+    @AfterTest
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun contains() {
+
+    }
+
+    @Test
+    fun doesNotContain() {
+
+    }
+}
